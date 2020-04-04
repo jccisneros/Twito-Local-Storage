d472157556b897d1ac42fcb860d2c1cb2e479ad8
Jose Castro Cisneros <jose_castro_cisneros@hotmail.es>
2020-04-04 17:08:41 -0500

    funciones de localStorage agregadas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,117 @@
+//app
 //variables
-
 const listaTweets = document.getElementById('lista-tweets');
 
-
 //funciones
-
 const agregarTweet = (e) => {
 
   e.preventDefault();
 
   const tweet = document.getElementById('tweet').value;
 
-  const botonBorrar = document.createElement('a');
-  botonBorrar.classList = 'mx-3 text-danger font-weight-bold';   
-  botonBorrar.id = 'borrar-tweet'
-  botonBorrar.innerText = 'X';
+  if (tweet == '') {
+    alert('escriba un tweet');
+  }else{
+    const botonBorrar = document.createElement('a');
+    botonBorrar.classList = 'mx-3 text-danger font-weight-bold';   
+    botonBorrar.id = 'borrar-tweet'
+    botonBorrar.innerText = 'X';
 
-  const li = document.createElement('li');  
-  li.innerText = tweet;  
-  li.classList = 'li-grid py-2'
+    const li = document.createElement('li');  
+    li.innerText = tweet;  
+    li.classList = 'li-grid py-2'
+    li.appendChild(botonBorrar);
+    listaTweets.appendChild(li);    
 
-  li.appendChild(botonBorrar);
-  listaTweets.appendChild(li);     
+    agregarTweetLocalStorage(tweet);
+  } 
 
-} 
+}
 
 const borrarTweet = (e) => {
 
   e.preventDefault;  
 
-  if(e.target.id == 'borrar-tweet') {
-    
-    e.target.parentElement.remove();
-    
-  }  
+  if(e.target.id === 'borrar-tweet') {    
 
+    e.target.parentElement.remove();
+    borrarTweetLocalStorage(e.target.parentElement.innerText);        
+  }  
+}
+
+//localStorage
+//funciones
+const localStorageReady = () => {
+
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();  
+
+  tweets.forEach( (tweet) => {
+
+    const botonBorrar = document.createElement('a');
+    botonBorrar.classList = 'mx-3 text-danger font-weight-bold';   
+    botonBorrar.id = 'borrar-tweet'
+    botonBorrar.innerText = 'X';
+
+    const li = document.createElement('li');  
+    li.innerText = tweet;  
+    li.classList = 'li-grid py-2'
+    li.appendChild(botonBorrar);
+    listaTweets.appendChild(li);  
+
+  });
+  
+}
+
+const agregarTweetLocalStorage = (tweet) =>{
+
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+  tweets.push(tweet);
+
+  localStorage.setItem('tweets', JSON.stringify(tweets) ); 
+
+};
+
+const obtenerTweetsLocalStorage = () =>{
+
+  let tweets;
+
+  if (localStorage.getItem('tweets') === null){    
+    tweets = [];    
+  }else{
+    tweets = JSON.parse( localStorage.getItem('tweets') );
+  }
+
+  return tweets;
+
+};
+
+borrarTweetLocalStorage = (tweet) => {
+
+  let  tweets,
+       borrarTweet;
+
+  tweets = obtenerTweetsLocalStorage ();  
+  borrarTweet = tweet.substring(0, tweet.length - 1);
+
+  tweets.forEach( (tweet, i) => {    
+    
+    if(borrarTweet === tweet){
+      tweets.splice(i, 1);
+    }
+  }); 
+  localStorage.setItem('tweets', JSON.stringify(tweets) );    
 }
 
 //eventListener
-
 const listeners = () => {
 
   const formulario = document.getElementById('formulario');
-  formulario.addEventListener('submit', agregarTweet);
 
-  listaTweets.addEventListener('click', borrarTweet);
+  formulario.addEventListener('submit', agregarTweet);
+  listaTweets.addEventListener('click', borrarTweet); 
+  document.addEventListener('DOMContentLoaded', localStorageReady);
   
 };
 
